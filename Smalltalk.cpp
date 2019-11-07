fa6e3fe961fff8240bfb077e6712a8eb07cbfb02
yeckoh <yeckoh@gmail.com>
2019-11-07 15:11:24 -0800

    saysomething circles, % is expensive according to the internet

--- Smalltalk.cpp
+++ Smalltalk.cpp
@@ -7,46 +7,37 @@
 
 #include <string>
 #include <vector>
+
 #include "./includes/Smalltalk.h"
 #include "./includes/constants.h"
 
 using namespace std;
 
-Smalltalk::Smalltalk(string myNationality,int iPerson) : nationality(myNationality), iPerson(1), current_phrase(0) {
-	this->pWatch = 0;
-}
+Smalltalk::Smalltalk(string myNationality,int iPerson) : nationality(myNationality), iPerson(1), current_phrase(0) {}
 
 Smalltalk::~Smalltalk(void) {
 
 }
 
-	//cycles through phrases added in populatePhrases. Returns them 1 by 1 starting with the first and ending
-	//with the last and then it starts over
-	//takes the form Nationality iPerson: phrase
-	//for instance the following string comes from an American instance, the 10th iPerson and it is printing AMERICAN_PHRASE_2
-	//AMERICAN 10:Why yes, I would like to supersize that
 	string Smalltalk::saySomething() {
-		return this->nationality + to_string(iPerson) + this->mySmallTalk[this->current_phrase];
+		string app = nationality + " " + to_string(iPerson) + ':' + mySmallTalk[current_phrase++];
+		if(current_phrase == mySmallTalk.size())
+			current_phrase = 0;
+		return app;
 	}
 
-	//returns the time (if pWatch contains a watch ) in the form of THE_CURRENT_TIME_IS: (from the actual watch object itself) and then the time
-	//or I_DO_NOT_HAVE_A_WATCH string (if pWatch does not contain a watch)
 	string Smalltalk::getTime() {
 		if(!pWatch)
 			return I_DO_NOT_HAVE_A_WATCH;
 		return pWatch->getTime();
 	}
 
-	//if this object has a watch it is taken away, otherwise an empty unique_ptr is returned
-	// This transaction simulates giving away a watch
 	unique_ptr<Watch> Smalltalk::takeWatch() {
 		if(!pWatch)
 			return 0;
 		return move(pWatch);
 	}
 
-	//if already have a watch then return false and dont change pWatch pointer
-	//otherwise accept watch (return true) and set this->pWatch=pWatch (use std::move)
 	bool Smalltalk::giveWatch(unique_ptr<Watch> &pWatch) {
 		if(!pWatch)
 			return false;
@@ -54,7 +45,6 @@ Smalltalk::~Smalltalk(void) {
 		return true;
 	}
 
-	//Abstract Base Class (ABC), implement in derived classes
-	// TODO: pure virual functions in base class implementation y/n
+	// TODO: destructors, memleaks, valgrind,
 	//virtual void Smalltalk::populatePhrases()=0;
 
