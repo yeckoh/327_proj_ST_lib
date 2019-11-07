49907660706358e1d362437cb816827eeb6018f7
yeckoh <yeckoh@gmail.com>
2019-11-07 15:10:16 -0800

    watch handouts

--- Functions.cpp
+++ Functions.cpp
@@ -28,44 +28,24 @@ vector<unique_ptr<Smalltalk>> getPeople(int numBrit,
 	vector<unique_ptr<Smalltalk>> people;
 
 		//add brits to vector
-	unique_ptr<Smalltalk> person;
-	for(int i = 0; i < numBrit; ++i) {
-//		person = std::unique_ptr<Smalltalk>(new Smalltalk_Brit(i));
-//		people.push_back(person);
-//		people.push_back(person = new Smalltalk_Brit(i));
+	for(int i = 0; i < numBrit; ++i)
 		people.push_back(std::unique_ptr<Smalltalk>(new Smalltalk_Brit(i)));
 
-	}
-
 		//add americans  to vector
-	for(int i = 0; i < numAmerican; ++i) {
-		//person = std::unique_ptr<Smalltalk>(new Smalltalk_American(i));
-		//people.push_back(person);
+	for(int i = 0; i < numAmerican; ++i)
 		people.push_back(std::unique_ptr<Smalltalk>(new Smalltalk_American(i)));
-	}
 
 		//add american donut enthusiest to vector
-	for(int i = 0; i < numbAmericanDonutEnthusiest; ++i) {
-		//person = std::unique_ptr<Smalltalk>(new ST_American_DonutEnthusiest(i));
-		//people.push_back(person);
+	for(int i = 0; i < numbAmericanDonutEnthusiest; ++i)
 		people.push_back(std::unique_ptr<Smalltalk>(new ST_American_DonutEnthusiest(i)));
-	}
 
 		//create some watches (as long as number watches <= numb people)
 	if(numWatches > people.size())
-		numWatches = people.size();
+		numWatches = people.size(); // looks passed by value, ok
+
 		//then give the watches away to first NUM_WATCHES people in the vector
+	for(int i = 0; i < numWatches; ++i)
+		people[i]->giveWatch(*new unique_ptr<Watch>(new Watch()));
 
-	unique_ptr<Watch>* handout;
-	for(int i = 0; i < numWatches; ++i) {
-		handout = new unique_ptr<Watch>(new Watch());
-		people[i]->giveWatch(*handout);
-		//people[i]->giveWatch(std::unique_ptr<Watch>(new Watch()));
-	}
-
-		// when you are finished using the vector you return
-		//from this function(see Smalltalk header for hints)
-
-		//return your vector
 	return people;
 }
